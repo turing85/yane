d3dfd38dd322e0c61bbc42838f3c8c9d8b5dc2f9
Marco Bungart <marco.bungart@googlemail.com>
2022-03-28 17:49:20 +0200

    Updated Cpu.java

--- yane-core/src/main/java/de/turing85/yane/api/Cpu.java
+++ yane-core/src/main/java/de/turing85/yane/api/Cpu.java
@@ -4,4 +4,17 @@ import java.util.*;
 
 public interface Cpu<I extends Instruction> {
   Set<I> instructions();
+  /**
+   * Resets the CPU in a know state.
+   *
+   * <p>In most circumstances, the reset state is hardwired in the CPU itself.
+   */
+  void reset();
+
+  /**
+   * Executes one CPU-cycle on the CPU.
+   *
+   * @return {@code true}, iff. the executed cycle competed a command.
+   */
+  boolean tick();
 }
