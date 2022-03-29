d54150c183d9b6f614b2e5fffe7f3cb3129a469d
Marco Bungart <marco.bungart@googlemail.com>
2022-03-29 21:48:02 +0200

    Added documentation to AddressingModeFunction

--- yane-core/src/main/java/de/turing85/yane/impl/cpu6502/AddressingModeFunction.java
+++ yane-core/src/main/java/de/turing85/yane/impl/cpu6502/AddressingModeFunction.java
@@ -3,7 +3,30 @@ package de.turing85.yane.impl.cpu6502;
 import de.turing85.yane.api.*;
 import java.util.function.*;
 
+/**
+ * <p>Represents a function that fetches the data from the {@link Register} and/or the{@link
+ * CpuBus}.</p>
+ *
+ * <p>Normally, data is fetched from the bus. There are some instructions, however, that require
+ * data from the register, possibly not hitting the bus at all (e.g. {@link Command#TAX}, which
+ * needs the value of {@link Register#a}.</p>
+ *
+ * <p>Furthermore, data fetches may occur on the {@link Register#programCounter}. In those cases,
+ * the program counter must be incremented.</p>
+ *
+ * <p>The call only represents data <strong>fetchin</strong>. If, for example, during the execution
+ * of a command is then subsequently written back to the bus, then this is not represented by this
+ * call.</p>
+ */
 interface AddressingModeFunction extends BiFunction<Register, CpuBus, AddressingResult> {
+
+  /**
+   * Reads the necessary data from the register and/or bus.
+   *
+   * @param register the register to fetch from
+   * @param bus the bus to fetch from
+   * @return the result of the fetch
+   */
   default AddressingResult fetch(Register register, CpuBus bus) {
     return apply(register, bus);
   }
